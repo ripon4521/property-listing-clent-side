5dd2e15e5e937b2aa23da08a6f47760a8412f44e
ripon4521 <riponakondo4521@gmail.com>
2024-05-01 12:14:37 +0600

    maked update tab data on supercharge section on production level

--- src/pages/Home/SuperCharge/TabData/TabData1/TabData1.jsx
+++ src/pages/Home/SuperCharge/TabData/TabData1/TabData1.jsx
@@ -5,8 +5,8 @@ const TabData1 = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2  lg:px-0  ">
           <div className="mt-10 ">
            <a href="#" className="flex w-full md:w-full  items-center bg-white hover:shadow-inner   rounded-lg shadow md:flex-row md:max-w-xl   ">
-    <div className="md:w-[600px] md:h-48 w-[300px]  bg-[#EFF9FF] flex justify-center items-center">
-        <img className=" w-full md:object-cover  h-48   rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://www.propertyfinder.ae/static/images/homepage/cards/Verified_Listings.svg" alt="" />
+    <div className="md:w-[600px] md:h-48 w-[300px]   flex justify-center items-center">
+        <img className=" w-full md:object-cover  h-48   rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://img.freepik.com/free-vector/building-permit-concept-illustration_114360-7579.jpg?t=st=1714543759~exp=1714547359~hmac=6b72eb0abed068c90ca7d0f899c80cdcfbcad36e2e54d4d05a4ea7db6412fa1c&w=740" alt="" />
     </div>
     <div className="flex flex-col justify-between p-4 md:px-10 ">
         <h5 className="mb-2  font-opanSans font-semibold tracking-tight text-gray-500 ">Listing You Can Trust</h5>
@@ -19,8 +19,8 @@ const TabData1 = () => {
         
           <div className="mt-10 ">
            <a href="#" className="flex w-full md:w-full  items-center bg-white hover:shadow-inner   rounded-lg shadow md:flex-row md:max-w-xl   ">
-    <div className="md:w-[600px] md:h-48 w-[300px]  bg-[#EFF9FF] flex justify-center items-center">
-        <img className=" w-full md:object-cover  h-48   rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://www.propertyfinder.ae/static/images/homepage/cards/Verified_Listings.svg" alt="" />
+    <div className="md:w-[600px] md:h-48 w-[300px]   flex justify-center items-center">
+        <img className=" w-full md:object-cover  h-48   rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://img.freepik.com/free-vector/order-food-concept-illustration_114360-6860.jpg?t=st=1714543856~exp=1714547456~hmac=2312551666c8296309315878a0d960143f09b409be767a7c1b68b0a53d6d76cf&w=740" alt="" />
     </div>
     <div className="flex flex-col justify-between p-4 md:px-10 ">
         <h5 className="mb-2  font-opanSans font-semibold tracking-tight text-gray-500 ">Listing You Can Trust</h5>
@@ -33,8 +33,8 @@ const TabData1 = () => {
 
           <div className="mt-10 ">
            <a href="#" className="flex w-full md:w-full  items-center bg-white hover:shadow-inner   rounded-lg shadow md:flex-row md:max-w-xl   ">
-    <div className="md:w-[600px] md:h-48 w-[300px]  bg-[#EFF9FF] flex justify-center items-center">
-        <img className=" w-full md:object-cover  h-48   rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://www.propertyfinder.ae/static/images/homepage/cards/Verified_Listings.svg" alt="" />
+    <div className="md:w-[600px] md:h-48 w-[300px]   flex justify-center items-center">
+        <img className=" w-full md:object-cover  h-48   rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://img.freepik.com/free-vector/team-goals-concept-illustration_114360-5146.jpg?t=st=1714543898~exp=1714547498~hmac=3fde3dd826a7810fe5f5e2d4413919026e8b0f19ac5d0af71aaaf61a7f32b95b&w=740" alt="" />
     </div>
     <div className="flex flex-col justify-between p-4 md:px-10 ">
         <h5 className="mb-2  font-opanSans font-semibold tracking-tight text-gray-500 ">Listing You Can Trust</h5>
@@ -47,8 +47,8 @@ const TabData1 = () => {
 
           <div className="mt-10 ">
            <a href="#" className="flex w-full md:w-full  items-center bg-white hover:shadow-inner   rounded-lg shadow md:flex-row md:max-w-xl   ">
-    <div className="md:w-[600px] md:h-48 w-[300px]  bg-[#EFF9FF] flex justify-center items-center">
-        <img className=" w-full md:object-cover  h-48   rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://www.propertyfinder.ae/static/images/homepage/cards/Verified_Listings.svg" alt="" />
+    <div className="md:w-[600px] md:h-48 w-[300px]   flex justify-center items-center">
+        <img className=" w-full md:object-cover  h-48   rounded-t-lg  md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://img.freepik.com/free-vector/boycott-abstract-concept-illustration-political-program-consumer-activism-collective-behavior-cancel-culture-moral-purchasing-solidarity-action-public-protest_335657-1101.jpg?t=st=1714543997~exp=1714547597~hmac=9b2a30def6ec5a409335752c478a5e26f940efac75144f18e99ff0579f72d9e3&w=740" alt="" />
     </div>
     <div className="flex flex-col justify-between p-4 md:px-10 ">
         <h5 className="mb-2  font-opanSans font-semibold tracking-tight text-gray-500 ">Listing You Can Trust</h5>
